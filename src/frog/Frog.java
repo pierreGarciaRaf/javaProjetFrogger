024860796fcc09122fea90d03e56907a74606b4f
pierre <pierre.garcia@u-psud.fr>
2019-11-14 19:31:46 +0100

    win and lose function done with limitation of movement

--- src/frog/Frog.java
+++ src/frog/Frog.java
@@ -7,9 +7,9 @@ import gameCommons.IFrog;
 
 public class Frog implements IFrog {
 
-	private Game m_game;
-	private Case m_position;
-	private Direction m_direction;
+	private static Game m_game;
+	private static Case m_position;
+	private static Direction m_direction;
 
 	public Frog(Game game, Case position, Direction direction) {
 		m_game = game;
@@ -38,7 +38,28 @@ public class Frog implements IFrog {
 	public Direction getDirection() {
 		return m_direction;
 	}
-
+	/**
+	 * Returns a case that is within game bounds.
+	 * @param abs to limit with width
+	 * @param ord to limit with height
+	 * 
+	 * @return Case gives a case that is the closest to given coords, while being in bounds.
+	 */
+	private static Case limitedCase(int abs, int ord) {
+		while (abs > m_game.width-1) {
+			abs -= 1;
+		}
+		while (abs < 0) {
+			abs += 1;
+		}
+		while (ord > m_game.height-1) {
+			ord -= 1;
+		}
+		while (ord < 0) {
+			ord += 1;
+		}
+		return new Case(abs,ord);
+	}
 	/**
 	 * D�place la grenouille dans la direction donn�e et teste la fin de partie
 	 * 
@@ -47,16 +68,21 @@ public class Frog implements IFrog {
 	public void move(Direction key) {
 		switch (key) {
 		case up:
-			m_position = new Case(m_position.absc, m_position.ord + 1);
+			m_position = limitedCase(m_position.absc, m_position.ord + 1);
+			m_game.testWin();
+			m_game.testLose();
 			break;
 		case down:
-			m_position = new Case(m_position.absc, m_position.ord - 1);
+			m_position = limitedCase(m_position.absc, m_position.ord - 1);
+			m_game.testLose();
 			break;
 		case left:
-			m_position = new Case(m_position.absc - 1, m_position.ord);
+			m_position = limitedCase(m_position.absc - 1, m_position.ord);
+			m_game.testLose();
 			break;
 		case right:
-			m_position = new Case(m_position.absc + 1, m_position.ord);
+			m_position = limitedCase(m_position.absc + 1, m_position.ord);
+			m_game.testLose();
 			break;
 		}
 	}
