5a3e69d040e68e218e9450c30f8db034bf80fa1f
kauzarc <jujurolland91@gmail.com>
2019-11-21 17:29:00 +0100

    EnvInf and FrogInf added

---
+++ src/environment/EnvInf.java
@@ -0,0 +1,26 @@
+package environment;
+
+import gameCommons.Case;
+import gameCommons.IEnvironment;
+
+public class EnvInf implements IEnvironment	{
+
+	@Override
+	public boolean isSafe(Case c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isWinningPosition(Case c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/frog/FrogInf.java
@@ -0,0 +1,27 @@
+package frog;
+
+import gameCommons.Case;
+import gameCommons.Direction;
+import gameCommons.IFrog;
+
+public class FrogInf implements IFrog{
+
+	@Override
+	public Case getPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Direction getDirection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void move(Direction key) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
