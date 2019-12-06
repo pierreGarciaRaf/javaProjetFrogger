97e7cfdb91fe60cb2c99b717e29e193e1b3f1c78
pierre <pierre.garcia@u-psud.fr>
2019-12-06 13:17:47 +0100

    frogs doc

--- src/gameCommons/Frogs.java
+++ src/gameCommons/Frogs.java
@@ -4,11 +4,15 @@ import java.util.ArrayList;
 
 @SuppressWarnings("serial")
 public class Frogs extends ArrayList<IFrog> {
-
+	/**
+	 * generates empty list of frogs.
+	 */
 	Frogs() {
 		super(0);
 	}
-
+	/**
+	 * @return last frog position
+	 */
 	public Case lastPos() {
 		int minIdx = 0;
 		int idx = 0;
